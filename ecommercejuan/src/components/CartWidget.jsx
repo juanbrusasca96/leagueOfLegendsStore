57bc5a1995d7596fa789e1a63e7678b41c0c2b92
Juan Brusasca <juanbrusasca96@gmail.com>
2022-06-25 11:57:03 -0300

    segundo desafio

---
+++ ecommercejuan/src/components/CartWidget.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CartWidget() {
+    return (
+        <div className='divBtn'>
+            <a className="btn" href="#!" role="button"><i className="fas fa-shopping-cart fa-lg"></i></a>
+        </div>
+    )
+}
---
+++ ecommercejuan/src/components/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ItemListContainer({geeting}) {
+  return (
+    <div className='itemListContainer'>
+        <h1>{geeting}</h1>
+    </div>
+  )
+}
